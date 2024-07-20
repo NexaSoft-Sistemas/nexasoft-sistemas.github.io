442c5017eddd1b1acf4fd913f4163104eb5893fc
Fábio Mattes <fabiomattes2007@gmail.com>
2024-07-19 23:36:22 -0300

    Correção de responsividade

--- menu.js
+++ menu.js
@@ -2,18 +2,15 @@ let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.getElementById('overlay-menu')
 
-console.log(overlay)
-console.log(menu)
-console.log(btnMenu)
 
-btnMenu.addEventListener('click', ()=>{
+btnMenu.addEventListener('click', () => {
     menu.classList.add('abrir-menu')
 })
 
-menu.addEventListener('click', ()=>{
+menu.addEventListener('click', () => {
     menu.classList.remove('abrir-menu')
 })
 
-overlay.addEventListener('click', ()=>{
+overlay.addEventListener('click', () => {
     menu.classList.remove('abrir-menu')
 })
